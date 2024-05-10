a0f231e755bd6570cbc55b986db051efdcce9d7c
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-05-09 23:09:04 -0600

    Update install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,5 +16,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('😇', 'appinstalled', event);
+    console.log('😁', 'appinstalled', event);
 });
